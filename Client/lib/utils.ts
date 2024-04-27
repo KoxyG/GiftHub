556779c041303f3c4c4b65a05a8d22aded632796
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2024-04-27 17:28:36 +0100

    api key

--- Client/lib/utils.ts
+++ Client/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const COVALENT_API_KEY = process.env.NEXT_PUBLIC_COVALENT_API_KEY
+export const COVALENT_API_KEY = "cqt_rQh8KDbwVkJGKbmbpVXtkYPxpvpb"
 
 export const COLORS = {
   "slate": "#64748b",
