f67d427048170b07be13162b201b0b2846908b94
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2024-04-27 16:58:19 +0100

    working on the landing page

---
+++ Client/README.md
@@ -0,0 +1,1 @@
+# Gift-hub Frontend
---
+++ Client/config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "GiftHub",
+  description:
+    "Gift tokens to your users. 100% free. Open source. 100% free.",
+  mainNav: [
+
+  ],
+  links: {
+    telegram: "t.me/GiiftHub_bot.",
+    docs: "",
+  },
+}
