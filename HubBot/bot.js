abe02c9465ca84cb505e25d0f37586a69575faff
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2024-04-28 23:17:34 +0100

    added bot

---
+++ HubBot/bot.js
@@ -0,0 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config()
+
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; 
+
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+ 
+
+  bot.sendMessage(chatId, `New Bounty Created: ${msg.text}`);
+});
