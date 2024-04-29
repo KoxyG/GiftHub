ebdf967393051375a02563c4526b26ee716fb2d7
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2024-04-29 22:06:36 +0100

    fixing the ui

--- Client/app/Create/page.tsx
+++ Client/app/Create/page.tsx
@@ -1,17 +1,10 @@
 'use client'
-import { Button } from "@/components/ui/button"
+
 import { Flex } from "@radix-ui/themes"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useContext, useEffect, useState } from "react"
-import { WalletContext } from "@/lib/store"
-import { CovalentClient } from "@covalenthq/client-sdk"
-import { COVALENT_API_KEY} from "@/lib/utils"
-import { useRouter } from "next/navigation"
-import { useToast } from "@/components/ui/use-toast"
 
 
-export default function TransactionPage() {
+
+export default function CreatePage() {
    
 
     return (
